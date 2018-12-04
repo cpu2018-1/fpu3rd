3b94d45baf392aca8165a4542a0173fda11f33cf
cpu2018-1 <apapupx0405@gmail.com>
2018-12-04 15:22:41 +0900

    仕様変更

--- ftoi.v
+++ ftoi.v
@@ -9,22 +9,19 @@ module ftoi(
     assign ex = x[30:23];
     assign mx = x[22:0];
 
-    wire [31:0] mya;
-    assign mya = {1'b1,mx,8'b0};
+    wire [30:0] mya;
+    assign mya = {1'b1,mx,7'b0};
 
     wire [7:0] se;
     assign se = 8'd157 - ex;
 
-    wire [31:0] myb;
-    assign myb = (mya >> se) + 1;
-  
-    wire [30:0] myc;
-    assign myc = myb[31:1];
+    wire [30:0] myb;
+    assign myb = mya >> se;
 
     wire sy;
     wire [30:0] my;
     assign sy = sx;
-    assign my = (sy) ? (~myc) + 1: myc;
+    assign my = (sy) ? (~myb) + 1: myb;
 
     assign y = (my == 0) ? 0: {sy,my};
 
